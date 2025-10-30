1ea512805bed6eaca26524755cce8cecc68def3a
N9 <adrianjohnversoza@gmail.com>
2025-10-30 15:11:05 +0800

    feat(analytics): Add Vercel Speed Insights
    This commit integrates Vercel Speed Insights into the application to monitor and analyze its performance.
    
    The `@vercel/speed-insights` package has been added as a dependency, and the `SpeedInsights` component is now included in the root layout alongside Vercel Analytics. This enables the collection of real-time performance data and Core Web Vitals, providing valuable insights for optimizing the user experience.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import 'react-big-calendar/lib/css/react-big-calendar.css'
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -23,6 +24,7 @@ export default function RootLayout({
           {children}
         </div>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   )
